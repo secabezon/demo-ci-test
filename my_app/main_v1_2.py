e41739fd39e0bad0a6717aa49e3c183cca29df12
sebastian Cabezon <secabezon21@gmail.com>
2024-10-22 22:14:40 -0300

    test ci2

--- my_app/main_v1_2.py
+++ my_app/main_v1_2.py
@@ -1,17 +1,22 @@
 from typing import List, Any
 
+
 def process_data(data):
     return data
 
-def concatenate_strings(string: List[str])-Z str:
+
+def concatenate_strings(string: List[str])-> str:
     return ''.join(string)
+
 
 def display_message(message: str):
     print(message)
 
+
 def main():
     data = process_data('example')
     print(data)
+
 
 if __name__=="__main__":
     main()
